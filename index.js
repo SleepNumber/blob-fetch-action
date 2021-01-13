8db3df6c191231a0e6f5aa926b4e34bd36ba98ed
mquintsn <mary.quint@sleepnumber.com>
2021-01-13 08:42:14 -0600

    remove json files

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const {
   
   const fs = require("fs");
   
-  // if (process.env.NODE_ENV !== "production") {
-  //   require("dotenv").config();
-  // }
-  
   const STORAGE_ACCOUNT_NAME = core.getInput('storage_account_name');
   const ACCOUNT_ACCESS_KEY = core.getInput('account_access_key');
   const CONTAINER_NAME = core.getInput('container_name');
